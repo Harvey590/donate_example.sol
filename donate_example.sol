39c769f07bd270bb25ba409c02a277a5281d3149
Harv <120187440+Harvey590@users.noreply.github.com>
2023-01-31 21:46:36 +0200

    Create donate_example.sol

---
+++ donate_example.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract HarvSC {
+
+    address public owner;
+    mapping (address => uint) public payments;
+
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    function DonateTo() public payable {
+        payments[msg.sender] = msg.value;
+    }
+
+    function TakeMoneyBack() public {
+        address payable _to = payable(owner);
+        address _thisContract = address(this);
+        _to.transfer(_thisContract.balance);
+    }
+}
